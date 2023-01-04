57c2576b8a1888c9796d08ccc93b71ffad0f4514
jubayerasif <jubaerasif101@gmail.com>
2023-01-04 23:40:01 +0600

    var

--- Day 1/js/main.js
+++ Day 1/js/main.js
@@ -2,8 +2,13 @@
 // console.log("This is in Console");
 
 // promt(""); is used to get data from client
+// var x;
+// x = prompt("Enter your name: ");
+// console.log(x);
+// document.write(x);
 
-var x;
-x = prompt("Enter your name: ");
-console.log(x);
-document.write(x);
+// var
+var a = 20;
+var b = 15;
+var c = a + b;
+console.log(c);
