68793f2e4cbdab5104b088a289df4b220b2929d8
jubayerasif <jubaerasif101@gmail.com>
2023-01-04 23:30:14 +0600

    user data

--- Day 1/js/main.js
+++ Day 1/js/main.js
@@ -1,3 +1,9 @@
-document.getElementById("p").innerHTML = "Learning Deep Javascript";
+// document.getElementById("p").innerHTML = 'Learning Deep Javascript';
+// console.log("This is in Console");
 
-        console.log("This is in Console");
+// promt(""); is used to get data from client
+
+var x;
+x = prompt("Enter your name: ");
+console.log(x);
+document.write(x);
