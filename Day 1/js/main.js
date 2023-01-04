5e4b9fbb83f83e5a7984ad5a92bb1906b4207896
jubayerasif <jubaerasif101@gmail.com>
2023-01-05 00:24:39 +0600

    operator done

--- Day 1/js/main.js
+++ Day 1/js/main.js
@@ -19,4 +19,8 @@
 // 2 + 3
 // + - * / %
 
-
+// Practice
+var temp = prompt("Please enter a temperature: ");
+var result = 9/5*temp + 32;
+alert("Fahrenheit: " + result + "Degree");
+console.log("Task Complete!");
