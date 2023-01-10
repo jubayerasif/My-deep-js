dacf3e4650505ce33a87d239a8bec9f4d9dde782
jubayerasif <jubaerasif101@gmail.com>
2023-01-10 12:30:47 +0600

    Math

--- Day 2/js/main.js
+++ Day 2/js/main.js
@@ -1,3 +1,5 @@
 var number = 5.4999999;
-var result = Math.round(number);
+// var result = Math.round(number);
+// var result = Math.ceil(number);
+var result = Math.floor(number);
 console.log(result);
