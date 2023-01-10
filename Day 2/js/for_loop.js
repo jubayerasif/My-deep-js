502afaef239d49f166653dfd40bab0cc4e71f2ac
jubayerasif <jubaerasif101@gmail.com>
2023-01-10 23:45:54 +0600

    for loop

---
+++ Day 2/js/for_loop.js
@@ -0,0 +1,9 @@
+for(var i = 0; i <= 11; i++){
+    console.log(i);
+}
+
+var nums = [55, 75, 45, 34, 52, 12, 38];
+for(var i = 0; i < nums.length; i++){
+    var element = nums[i];
+    console.log(element);
+}
