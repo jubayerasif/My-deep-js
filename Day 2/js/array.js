0d6061ea3aa2616539ba6bfdfcb1c5ff5ba683a8
jubayerasif <jubaerasif101@gmail.com>
2023-01-10 19:12:12 +0600

    index of

--- Day 2/js/array.js
+++ Day 2/js/array.js
@@ -1,5 +1,7 @@
-var student_age = [17, 15, 16, 18, 20]
-var asif_age = student_age[3]
-student_age[1] = 2
+var student_age = [17, 15, 16, 18, 20];
+var asif_age = student_age[3];
+student_age[1] = 2;
+var position = student_age.indexOf(20);
 console.log(student_age);
 console.log(asif_age);
+console.log(position);
