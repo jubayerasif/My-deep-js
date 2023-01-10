b90706bc68353f0612233e9ea05d049ec594d92f
jubayerasif <jubaerasif101@gmail.com>
2023-01-10 12:58:58 +0600

    2

--- Day 2/js/conditional.js
+++ Day 2/js/conditional.js
@@ -1,7 +1,8 @@
-var project_price = 800;
-if (project_price != 100){
-    console.log("I will take the Project");
-}
-else{
-    console.log("I won't take the Project");
-}
+// var project_price = 800;
+// if (project_price != 100){
+//     console.log("I will take the Project");
+// }
+// else{
+//     console.log("I won't take the Project");
+// }
+
