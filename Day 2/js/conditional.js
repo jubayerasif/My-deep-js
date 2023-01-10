5d2bfb18665254ffab8082f3f7cbf2a476de8432
jubayerasif <jubaerasif101@gmail.com>
2023-01-10 13:03:11 +0600

    Job code

--- Day 2/js/conditional.js
+++ Day 2/js/conditional.js
@@ -6,3 +6,12 @@
 //     console.log("I won't take the Project");
 // }
 
+var cgpa = 2.75;
+var savings = 1000000;
+if(cgpa == 3.75 && savings > 1500000){
+    console.log("You will get a job");
+}else if(cgpa == 3.75){
+    console.log("You will be considered");
+}else{
+    console.log("Sorry, Better luck next time")
+}
