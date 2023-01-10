4fb6545f6caaf76039dad31eceb83295d8d8b6cb
jubayerasif <jubaerasif101@gmail.com>
2023-01-10 12:38:25 +0600

    random Math

--- Day 2/js/main.js
+++ Day 2/js/main.js
@@ -1,5 +1,7 @@
-var number = 5.4999999;
+// var number = 5.4999999;
 // var result = Math.round(number);
 // var result = Math.ceil(number);
-var result = Math.floor(number);
+// var result = Math.floor(number);
+var number = Math.random() * 100;
+var result = Math.round(number);
 console.log(result);
