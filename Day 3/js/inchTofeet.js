17a0cc5cd7b9546af51dd1d4586d270aa92d56e1
jubayerasif <jubaerasif101@gmail.com>
2023-01-11 00:58:32 +0600

    inch to feet done

---
+++ Day 3/js/inchTofeet.js
@@ -0,0 +1,12 @@
+// var inch = 156;
+// var feet = inch / 12;
+// console.log(feet);
+
+function inchTofeet(inch){
+    var feet = inch / 12;
+    return feet;
+}
+var mainfeet = inchTofeet(156);
+console.log(mainfeet);
+var mainfeet2 = inchTofeet(288);
+console.log(mainfeet2);
