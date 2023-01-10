50184760edc5c2dc40b6f064451044af7c7952d9
jubayerasif <jubaerasif101@gmail.com>
2023-01-10 23:46:31 +0600

    for loop done

--- Day 2/js/for_loop.js
+++ Day 2/js/for_loop.js
@@ -1,6 +1,6 @@
-for(var i = 0; i <= 11; i++){
-    console.log(i);
-}
+// for(var i = 0; i <= 11; i++){
+//     console.log(i);
+// }
 
 var nums = [55, 75, 45, 34, 52, 12, 38];
 for(var i = 0; i < nums.length; i++){
