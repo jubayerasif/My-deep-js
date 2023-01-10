69369eb80f72579c2a771e5a464ead1207e4d236
jubayerasif <jubaerasif101@gmail.com>
2023-01-10 12:29:42 +0600

    number

--- Day 2/js/main.js
+++ Day 2/js/main.js
@@ -1,35 +1,3 @@
-// document.getElementById("p").innerHTML = 'Learning Deep Javascript';
-// console.log("This is in Console");
-
-// promt(""); is used to get data from client
-// var x;
-// x = prompt("Enter your name: ");
-// console.log(x);
-// document.write(x);
-
-// var variable value can be changed
-// var a = 20;
-// var b = 15;
-// var c = a + b;
-// console.log(c);
-// const constant value can't be changed
-
-// Operator
-// operand operator operand
-// 2 + 3
-// + - * / %
-
-// Practice
-// var temp = prompt("Please enter a temperature: ");
-// var result = 9/5*temp + 32;
-// alert("Fahrenheit: " + result + "Degree");
-// console.log("Task Complete!");
-
-
-// String
-x = "Hello \\ World";
-console.log(x);
-x = "Hello \t World";
-console.log(x);
-x = "Hello \n World";
-console.log(x);
+var number = 5.4999999;
+var result = Math.round(number);
+console.log(result);
