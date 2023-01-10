b1e8e16c7c8c466f0ab6d5eb83f30dd15438e611
jubayerasif <jubaerasif101@gmail.com>
2023-01-11 00:43:30 +0600

    object.js added plus done

---
+++ Day 3/js/object.js
@@ -0,0 +1,17 @@
+var student1 = {id: 3245, name:"Mahir", phone:456456}
+var student2 = {id: 4225, name:"Mahir", phone:588657}
+
+var phoneNum1 = student1["phone"];
+var phoneNum2 = student1.phone;
+var phoneNum0 = "phone";
+var phoneNum3 = student1[phoneNum0];
+
+console.log(student1);
+console.log(student2);
+console.log(phoneNum1);
+console.log(phoneNum2);
+console.log(phoneNum3);
+
+
+
+
