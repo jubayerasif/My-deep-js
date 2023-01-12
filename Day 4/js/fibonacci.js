cbbba4cd154aa907f9780779e2dbb3dca1d56e5a
jubayerasif <jubaerasif101@gmail.com>
2023-01-13 01:00:51 +0600

    fibonacciseriesrecursive.js done

--- Day 4/js/fibonacci.js
+++ Day 4/js/fibonacci.js
@@ -1,7 +1,7 @@
 function fibonacci(n) {
-    let fibo = [0, 1]
+    let fibo = [0, 1];
     for(let i = 2; i <= n; i++){
-        fibo[i]= fibo[i - 1] + fibo [i - 2]
+        fibo[i]= fibo[i - 1] + fibo [i - 2];
     }
     return fibo;
 }
