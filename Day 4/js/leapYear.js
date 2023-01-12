93074396c6d2a3a3debe1f0dccae2934a99b96d7
jubayerasif <jubaerasif101@gmail.com>
2023-01-12 23:55:53 +0600

    practising leap year

---
+++ Day 4/js/leapYear.js
@@ -0,0 +1,7 @@
+// const year = 3688;
+// let reminder = year %4;
+// if(reminder == 0){
+//     console.log("This is a Leap Year");
+// } else {
+//     console.log("This is not a Leap Year");
+// }
