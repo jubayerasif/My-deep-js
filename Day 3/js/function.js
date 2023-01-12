6de6f4b1293dcc178aca879d014adf888b99a580
jubayerasif <jubaerasif101@gmail.com>
2023-01-12 23:42:27 +0600

    adding const in function.js

--- Day 3/js/function.js
+++ Day 3/js/function.js
@@ -11,8 +11,8 @@
 // doubleIt(5)
 
 function add(num1, num2){
-    var result = num1 + num2;
+    const result = num1 + num2;
     return result;
 }
-var sum = add(15, 25);
+const sum = add(15, 25);
 console.log(sum);
