171fdd8bd2e573bb7964721311917db37cf33ce0
jubayerasif <jubaerasif101@gmail.com>
2023-01-13 15:32:22 +0600

    math max

--- Day 4-2/swap.js
+++ Day 4-2/swap.js
@@ -1,10 +1,6 @@
-//JavaScript program to swap two variables
-//take input from the users
 let a = prompt('Enter the first variable: ');
 let b = prompt('Enter the second variable: ');
-//create a temporary variable
 let temp;
-//swap variables
 temp = a;
 a = b;
 b = temp;
