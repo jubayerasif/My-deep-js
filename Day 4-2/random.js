37298aed5dd49d479cba82cd65173c87ccab81a6
jubayerasif <jubaerasif101@gmail.com>
2023-01-13 15:21:50 +0600

    random and swap

---
+++ Day 4-2/random.js
@@ -0,0 +1,15 @@
+let num = 3.45675;
+let result = Math.floor(num);
+let result2 = Math.ceil(num);
+let result3 = Math.round(num);
+console.log(result);
+let dice = Math.random() * 6;
+console.log(dice);
+
+for(let i = 0; i <= 10; i++){
+    let randomNum = Math.random() * 10;
+    let output = Math.round(randomNum)
+    console.log(output);
+}
+
+
---
+++ Day 4-2/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let num = 3.45675;
+// let result = Math.floor(num);
+// let result2 = Math.ceil(num);
+// let result3 = Math.round(num);
+// console.log(result);
+// let dice = Math.random() * 6;
+// console.log(dice);
