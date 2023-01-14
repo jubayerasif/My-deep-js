c282a62d67c9f3a06a074345fdaecc7dfe0813c2
jubayerasif <jubaerasif101@gmail.com>
2023-01-14 23:54:40 +0600

    feetToMile.js added

---
+++ Day 6/main.js
@@ -0,0 +1,35 @@
+// document.getElementById("p").innerHTML = 'Learning Deep Javascript';
+// console.log("This is in Console");
+
+// promt(""); is used to get data from client
+// var x;
+// x = prompt("Enter your name: ");
+// console.log(x);
+// document.write(x);
+
+// var variable value can be changed
+// var a = 20;
+// var b = 15;
+// var c = a + b;
+// console.log(c);
+// const constant value can't be changed
+
+// Operator
+// operand operator operand
+// 2 + 3
+// + - * / %
+
+// Practice
+// var temp = prompt("Please enter a temperature: ");
+// var result = 9/5*temp + 32;
+// alert("Fahrenheit: " + result + "Degree");
+// console.log("Task Complete!");
+
+
+// String
+x = "Hello \\ World";
+console.log(x);
+x = "Hello \t World";
+console.log(x);
+x = "Hello \n World";
+console.log(x);
