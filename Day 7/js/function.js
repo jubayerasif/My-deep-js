f430bb8eb9812f21d88cf8c112b5ac681cba5303
jubayerasif <jubaerasif101@gmail.com>
2023-01-15 23:55:32 +0600

    function array

--- Day 7/js/function.js
+++ Day 7/js/function.js
@@ -1,32 +1,40 @@
-// function evenify(num){
-//     if(num % 2 == 0){
-//         console.log(num, `: is even number`);
-//     }else{
-//         console.log(num*2, `: is odd number`);
-//     }
-// }
-
 // function evenify_all(nums){
 //     for(let i = 0; i < nums.length; i++){
 //         const num = nums[i];
-//         evenify(num);
+//         if(num % 2 == 0){
+//             console.log(num, `: is even number`);
+//         }else{
+//             console.log(num*2, `: is odd number`);
+//         }
 //     } 
 // }
+
+function evenify(num){
+    let result;
+    if(num % 2 == 0){
+        result = num;
+    }else{
+        result = num*2;
+    }
+    return result;
+}
+
+let result = evenify(13);
+const square = result* result;
+console.log(`square: `, square);
 
 function evenify_all(nums){
     for(let i = 0; i < nums.length; i++){
         const num = nums[i];
-        if(num % 2 == 0){
-            console.log(num, `: is even number`);
-        }else{
-            console.log(num*2, `: is odd number`);
-        }
+        evenify(num);
     } 
 }
 
+
+
 let nums = [5, 12, 89, 45, 18, 8];
-evenify_all(nums);
+// evenify_all(nums);
 
 let friends_age = [13, 17, 19, 20, 18];
-evenify_all(friends_age)
+// evenify_all(friends_age)
 
