001066c377c64ad662e8e1131b1e7c9fb6824b44
jubayerasif <jubaerasif101@gmail.com>
2023-01-16 00:19:07 +0600

    some few changes in callback.js

--- Day 7/js/callback.js
+++ Day 7/js/callback.js
@@ -1,21 +1,21 @@
 function explainCallback(name, age, task){
-    console.log(`Your name:`, name);
-    console.log(`Your age:`, age);
+    console.log(`Name:`, name);
+    console.log(`Age:`, age);
     task();
 }
 function studyHard(){
-    console.log(`Studies more than 5 hours`);
+    console.log(`Task: Studies more than 5 hours`);
 }
 function takeBreak(){
-    console.log(`Takes break for only half hours`);
+    console.log(`Task: Takes break for only half hours`);
 }
 function playTime(){
-    console.log(`Plays only Cricket`);
+    console.log(`Task: Plays only Cricket`);
 }
 
 explainCallback('Asifur Hasan', 17, studyHard);
-explainCallback('Asifur Hasan', 17, takeBreak);
-explainCallback('Asifur Hasan', 17, playTime);
+explainCallback('Taskin Hasan', 17, takeBreak);
+explainCallback('Sakib AL Hasan', 17, playTime);
 
 
 
