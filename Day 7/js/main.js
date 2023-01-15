4108561f5fe65b14e80687bb7b8e6e332a532fe7
jubayerasif <jubaerasif101@gmail.com>
2023-01-16 01:33:35 +0600

    using Event Listener

--- Day 7/js/main.js
+++ Day 7/js/main.js
@@ -1,4 +1,19 @@
-function handleCLick(){
-    // console.log("Someone Clicked Me");
-    console.log(new Date());
-}
+// function handleClick(){
+//     // console.log("Someone Clicked Me");
+//     // console.log(new Date());
+//     document.body.style.backgroundColor = 'Pink';
+// }
+// const secondButton = document.getElementById('second');
+// // secondButton.onclick = handleClick;
+// // secondButton.addEventListener('click', handleClick)
+
+const lastButton = document.getElementById('last').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'Yellow';
+})
+
+
+
+
+
+
+
