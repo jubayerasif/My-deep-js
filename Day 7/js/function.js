f8247394c961b2fa2e539c1d858501d5afa6ba1e
jubayerasif <jubaerasif101@gmail.com>
2023-01-15 23:57:25 +0600

    function array

--- Day 7/js/function.js
+++ Day 7/js/function.js
@@ -24,16 +24,19 @@ const square = result* result;
 console.log(`square: `, square);
 
 function evenify_all(nums){
+    let even_array = [];
     for(let i = 0; i < nums.length; i++){
         const num = nums[i];
-        evenify(num);
+        let result = evenify(num);
+        even_array.push(result);
     } 
+    return even_array;
 }
 
 
 
 let nums = [5, 12, 89, 45, 18, 8];
-// evenify_all(nums);
+evenify_all(nums);
 
 let friends_age = [13, 17, 19, 20, 18];
 // evenify_all(friends_age)
