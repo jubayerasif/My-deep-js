2f7ef02ea78a1c253cca2b4737bbb642065f63bd
jubayerasif <jubaerasif101@gmail.com>
2023-01-15 23:35:06 +0600

    friend_age

--- Day 7/js/function.js
+++ Day 7/js/function.js
@@ -1,9 +1,23 @@
+
+
+
+
 let nums = [5, 12, 45, 89, 18, 8];
 for(let i = 0; i < nums.length; i++){
     const num = nums[i];
     if(num % 2 == 0){
         console.log(num, `: is even number`);
     }else{
-        console.log(num*2, `: is odd number`);
+        console.log(num, `: is odd number`);
     }
 } 
+
+let friends_age = [18, 17, 16, 25, 22, 20];
+for (let i = 0; i < friends_age.length; i++) {
+    const age = friends_age[i];
+    if(age % 2 == 0){
+        console.log(age, `: is even number`);
+    }else{
+        console.log(age, `: is odd number`);
+    }
+}
