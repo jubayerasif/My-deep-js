2f181ffd74a56b35183b94373daf95d40abef77b
jubayerasif <jubaerasif101@gmail.com>
2023-01-15 23:45:26 +0600

    function loop

--- Day 7/js/function.js
+++ Day 7/js/function.js
@@ -1,23 +1,32 @@
+// function evenify(num){
+//     if(num % 2 == 0){
+//         console.log(num, `: is even number`);
+//     }else{
+//         console.log(num*2, `: is odd number`);
+//     }
+// }
 
+// function evenify_all(nums){
+//     for(let i = 0; i < nums.length; i++){
+//         const num = nums[i];
+//         evenify(num);
+//     } 
+// }
 
-
-
-let nums = [5, 12, 45, 89, 18, 8];
-for(let i = 0; i < nums.length; i++){
-    const num = nums[i];
-    if(num % 2 == 0){
-        console.log(num, `: is even number`);
-    }else{
-        console.log(num, `: is odd number`);
-    }
-} 
-
-let friends_age = [18, 17, 16, 25, 22, 20];
-for (let i = 0; i < friends_age.length; i++) {
-    const age = friends_age[i];
-    if(age % 2 == 0){
-        console.log(age, `: is even number`);
-    }else{
-        console.log(age, `: is odd number`);
-    }
+function evenify_all(nums){
+    for(let i = 0; i < nums.length; i++){
+        const num = nums[i];
+        if(num % 2 == 0){
+            console.log(num, `: is even number`);
+        }else{
+            console.log(num*2, `: is odd number`);
+        }
+    } 
 }
+
+let nums = [5, 12, 89, 45, 18, 8];
+evenify_all(nums);
+
+let friends_age = [13, 17, 19, 20, 18];
+evenify_all(friends_age)
+
