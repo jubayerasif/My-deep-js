30f3bebd1ccdb9b1d9347de57745317868e2e5df
jubayerasif <jubaerasif101@gmail.com>
2023-01-15 16:10:19 +0600

    learned ELement and Query

--- Day 6/js/main.js
+++ Day 6/js/main.js
@@ -1,2 +1,7 @@
-let first = document.getElementById('first1')
-first.style.color = 'red';
+let authors = document.getElementsByClassName('author');
+for(let i = 0; i <authors.length; i++){
+    let element = authors[i];
+    console.log(element.innerHTML);
+    element.innerHTML = 'Writer-' + i;
+    element.style.backgroundColor = 'lightblue';
+}
