79128ec6af9144e8ae23ba002f557eb3810a723b
jubayerasif <jubaerasif101@gmail.com>
2023-01-22 01:03:51 +0600

    day 10 commit

--- Day 10/arguments.js
+++ Day 10/arguments.js
@@ -1,19 +1,15 @@
 // When and how to use arguments in a function
 
 function getFullName(firstName, lastName) {
-    // console.log(arguments);
-    // const fullName = firstName + ' ' + lastName;
-    let fullName = ' ';
-    for(let i = 0; i < arguments.length; i++) {
-        const namePart = arguments[i];
-        fullName = fullName + ' ' + namePart;
-    }
-    return fullName;
+  // console.log(arguments);
+  // const fullName = firstName + ' ' + lastName;
+  let fullName = " ";
+  for (let i = 0; i < arguments.length; i++) {
+    const namePart = arguments[i];
+    fullName = fullName + " " + namePart;
+  }
+  return fullName;
 }
 
-const name = getFullName('Jubayer','Hasan', 'Asif');
+const name = getFullName("Jubayer", "Hasan", "Asif");
 console.log(name);
-
-
-
-
--- Day 10/callBack.js
+++ Day 10/callBack.js
@@ -1,8 +1,23 @@
 //When and how to use Callback functions
 
-function welcomeGuest(name) {
-    console.log(name);
+// function welcomeGuest(name) {
+//     console.log(name);
+// }
+
+// const actorName = 'Bruce Wayne';
+// welcomeGuest(actorName);
+
+function welcomeGuest(name, greetHandler) {
+  greetHandler(name);
 }
 
-const actorName = 'Bruce Wayne';
-welcomeGuest(actorName);
+const actorName = "Bruce Wayne";
+
+function greetMorning(name) {
+  console.log("Good Morning", name);
+}
+function greetAfternoon(name) {
+  console.log("Good Afternoon", name);
+}
+
+welcomeGuest(actorName, greetAfternoon);
