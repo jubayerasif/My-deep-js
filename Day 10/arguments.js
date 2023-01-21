f4e5c4cfe667a1730a907ca9bb3669da86eec7b1
jubayerasif <jubaerasif101@gmail.com>
2023-01-22 00:52:27 +0600

    callback function added

--- Day 10/arguments.js
+++ Day 10/arguments.js
@@ -1,7 +1,13 @@
 // When and how to use arguments in a function
 
 function getFullName(firstName, lastName) {
-    const fullName = firstName + ' ' + lastName;
+    // console.log(arguments);
+    // const fullName = firstName + ' ' + lastName;
+    let fullName = ' ';
+    for(let i = 0; i < arguments.length; i++) {
+        const namePart = arguments[i];
+        fullName = fullName + ' ' + namePart;
+    }
     return fullName;
 }
 
---
+++ Day 10/callBack.js
@@ -0,0 +1,8 @@
+//When and how to use Callback functions
+
+function welcomeGuest(name) {
+    console.log(name);
+}
+
+const actorName = 'Bruce Wayne';
+welcomeGuest(actorName);
