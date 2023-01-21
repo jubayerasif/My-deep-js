92c8e551c3fafc08b543d7cd45419a4455f0057b
jubayerasif <jubaerasif101@gmail.com>
2023-01-22 00:35:34 +0600

    arguments.js added

---
+++ Day 10/arguments.js
@@ -0,0 +1,13 @@
+// When and how to use arguments in a function
+
+function getFullName(firstName, lastName) {
+    const fullName = firstName + ' ' + lastName;
+    return fullName;
+}
+
+const name = getFullName('Jubayer','Hasan', 'Asif');
+console.log(name);
+
+
+
+
