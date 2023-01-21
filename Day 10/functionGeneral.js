ba6479ebfee8877298dba49b210f615532c023d3
jubayerasif <jubaerasif101@gmail.com>
2023-01-22 00:26:58 +0600

    functionGeneral.js added

---
+++ Day 10/functionGeneral.js
@@ -0,0 +1,16 @@
+function addTodo(task) {
+    const newTaskElement = document.createElement('li')
+}
+// newTaskElement variable is local variable
+
+var myTodo = function(task){
+
+};
+
+// IIFE
+(function addUser() {
+    lawyerName = 'Rakib';
+    console.log(lawyerName);
+})()
+
+// addUser();
