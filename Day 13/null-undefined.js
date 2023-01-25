98d7e6ca749d5856c08b42e9af26cbb2effb70cd
jubayerasif <jubaerasif101@gmail.com>
2023-01-26 00:17:27 +0600

    null-undefined

---
+++ Day 13/null-undefined.js
@@ -0,0 +1,13 @@
+// function add(num1,num2){
+//     // console.log(num1 + num2);
+//     return num1 + num2;
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+// const result = add(12);
+// console.log(result);
+
+
