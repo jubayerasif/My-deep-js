5fcb68775bbfd88e5905f6ad1e3e322e5f1c4f28
jubayerasif <jubaerasif101@gmail.com>
2023-01-26 03:16:45 +0600

    obj - property

---
+++ Day 13/object-property.js
@@ -0,0 +1,20 @@
+const students = [
+    {id: 10, name: "Jubayer"},
+    {id: 20, name: "Hasan"},
+    {id: 30, name: "Asif"}
+]
+
+const names = students.map(s => s.name);
+const ids = students.map(s => s.id);
+const bigger = students.filter(s => s.id = 20)
+// console.log(names, ids);
+console.log(bigger);
+
+
+
+
+
+
+
+
+
