d3997ed62cb3f43474d4d76bb1757cacc4fd8020
jubayerasif <jubaerasif101@gmail.com>
2023-01-25 10:07:16 +0600

    es6 template

---
+++ Day 12/template.js
@@ -0,0 +1,12 @@
+const firstName = "Jubayer";
+const lastName = " Asif";
+const fullName = firstName + lastName + " is a programmer";
+const fullName2 = `${firstName} ${lastName} is a programmer`;
+console.log(fullName);
+
+
+
+
+
+
+
