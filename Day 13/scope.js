17a97fb88335c4afe8d4f38b8ab0043bb7d7cba4
jubayerasif <jubaerasif101@gmail.com>
2023-01-26 10:18:22 +0600

    scope in js

--- Day 13/scope.js
+++ Day 13/scope.js
@@ -1,17 +1,10 @@
-function sum(num1, num2){
-    let result = num1 + num2;
-    console.log(result);
-    return result;
+function sum(num1, num2) {
+  let result = num1 + num2;
+  console.log(result);
+  return result;
 }
 
 const output = sum(2, 2);
 console.log(output);
 
 // var is a global scope
-
-
-
-
-
-
-
