11557a83f4176107c4f49d0b638c82eaab8a04ee
jubayerasif <jubaerasif101@gmail.com>
2023-01-27 16:05:57 +0600

    day 14

---
+++ Day 14/callApply.js
@@ -0,0 +1,16 @@
+const normalPerson = {
+    firstName: 'Tom',
+    lastName: 'Smith',
+    salary: 25000;
+    getFullName: function(){
+        console.log(this.first, this.lastName);;
+    }
+}
+
+console.log(normalPerson.firstName, normalPerson.lastName);
+
+
+
+
+
+
