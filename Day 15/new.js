d8eec562ea4f4496ed941d03345badd26593e1a4
jubayerasif <jubaerasif101@gmail.com>
2023-01-29 00:07:23 +0600

    New JS

--- Day 15/new.js
+++ Day 15/new.js
@@ -1,8 +1,12 @@
 class Person {
-  constructor() {
+  constructor(firstName, lastName, salary) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.salary = salary;
   }
 }
-cont
+const heroPerson = new Person('Bruce', 'Wayne', 10000);
+console.log(heroPerson);
+
+const friendlyPerson = new Person('Robert', 'Wayne', 5000);
+console.log(friendlyPerson);
