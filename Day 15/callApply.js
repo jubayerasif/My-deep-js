5cac0d403b267ad6b6f0c71b99e8569b6573d419
jubayerasif <jubaerasif101@gmail.com>
2023-01-28 16:26:37 +0600

    bind and call

--- Day 15/callApply.js
+++ Day 15/callApply.js
@@ -5,14 +5,29 @@ const normalPerson = {
     getFullName: function () {
       console.log(this.first, this.lastName);
     },
-    chargeBill: function (amount) {
+    chargeBill: function (amount, tax, tips) {
       console.log(this);
-      this.salary = this.salary - amount;
+      this.salary = this.salary - amount - tax - tips ;
       return this.salary;
     }
   };
 
+const heroPerson = {
+  firstName: "Hero",
+  lastName: "Jerry",
+  salary: 41000,
+};
+// normalPerson.chargeBill();
 
 
 
-normalPerson
+// normalPerson.chargeBill.call(heroPerson, 900, 100, 10);
+// normalPerson.chargeBill.call(heroPerson, 100, 300, 30);
+// console.log(heroPerson.salary);
+
+// normalPerson.chargeBill.call(friendlyPerson, 900, 100, 10);
+// console.log(friendlyPerson.salary);
+
+normalPerson.chargeBill.apply(heroPerson, [500, 100, 10]);
+console.log(heroPerson.salary);
+
