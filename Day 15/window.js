8c7a087a9ae94ab3952b56b7f35641efa5a94f1f
jubayerasif <jubaerasif101@gmail.com>
2023-01-28 23:41:31 +0600

    making window

--- Day 15/window.js
+++ Day 15/window.js
@@ -1,7 +1,15 @@
+let name= 'Tom';
+
 function add(num1, num2){
     let result = num1 + num2;
     console.log('In result', result);
-    return result;
+    function double(num){
+        return num * 2;
+    }
+    // return result;
+    let total = double(result)
+    return total;
+
 }
 
 let sum = add(30, 20);
