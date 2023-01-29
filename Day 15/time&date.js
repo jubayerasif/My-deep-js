8c10bc39356c6bd290387f801ef67f1a5d6317c6
jubayerasif <jubaerasif101@gmail.com>
2023-01-29 10:31:43 +0600

    time&date in JS

--- Day 15/time&date.js
+++ Day 15/time&date.js
@@ -1,9 +1,7 @@
 const start = new Date();
 let sum = 0;
-for(let i = 0; i<10000000; i++){
-    sum++;
+for (let i = 0; i < 10000000; i++) {
+  sum++;
 }
 const end = new Date();
-console.log('time elapsed', end-start, sum);
-
-
+console.log("time elapsed", end - start, sum);
