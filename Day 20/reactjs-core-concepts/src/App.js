c78f7f14638fd301de495b851e675591ef86d6ff
jubayerasif <jubaerasif101@gmail.com>
2023-03-10 16:06:48 +0600

    using html and css in js

--- Day 20/reactjs-core-concepts/src/App.js
+++ Day 20/reactjs-core-concepts/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let person = {
+    name: "John Wick",
+    age: "35",
+    job: "Assassin",
+  };
+  let style = {
+    color: "yellow",
+    backgroundColor: "red",
+  };
+  let style2 = {
+    marginTop: "-15px",
+    marginBottom: "-25px"
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +31,15 @@ function App() {
         >
           Learn React
         </a>
+        <h4>My Heading: {(2 + 5) * 25 + 65}</h4>
+        <h1 style={style2}>Name: {person.name}</h1>
+        <h1 className="" style={style}>
+          Job: {person.job}
+        </h1>
+        {/* <p>My first React Paragraph</p> */}
       </header>
     </div>
   );
-}
+};
 
 export default App;
